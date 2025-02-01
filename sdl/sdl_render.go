272afaa885cca7ad1f26ff07019d26d4c253205f
Robin Eklind <rnd0x00@gmail.com>
2025-02-01 01:16:25 +0100

    render: add methods for Renderer and Texture types

--- sdl/sdl_render.go
+++ sdl/sdl_render.go
@@ -20,9 +20,19 @@ func SetRenderDrawColor(renderer *Renderer, r, g, b, a uint8) bool {
 	return sdlSetRenderDrawColor(renderer, r, g, b, a)
 }
 
+// SetDrawColor sets the color used for drawing operations.
+func (renderer *Renderer) SetDrawColor(r, g, b, a uint8) bool {
+	return SetRenderDrawColor(renderer, r, g, b, a)
+}
+
 // RenderPresent updates the screen with any rendering performed since the previous call.
 func RenderPresent(renderer *Renderer) bool {
 	return sdlRenderPresent(renderer)
+}
+
+// Present updates the screen with any rendering performed since the previous call.
+func (renderer *Renderer) Present() bool {
+	return RenderPresent(renderer)
 }
 
 // RenderClear clears the current rendering target with the drawing color.
@@ -30,9 +40,19 @@ func RenderClear(renderer *Renderer) bool {
 	return sdlRenderClear(renderer)
 }
 
+// Clear clears the current rendering target with the drawing color.
+func (renderer *Renderer) Clear() bool {
+	return RenderClear(renderer)
+}
+
 // DestroyRenderer destroys the rendering context for a window and free all associated textures.
 func DestroyRenderer(renderer *Renderer) {
 	sdlDestroyRenderer(renderer)
+}
+
+// Destroy destroys the rendering context for a window and free all associated textures.
+func (renderer *Renderer) Destroy() {
+	DestroyRenderer(renderer)
 }
 
 // RenderRect draws a rectangle on the current rendering target at subpixel precision.
@@ -40,9 +60,19 @@ func RenderRect(renderer *Renderer, rect *FRect) bool {
 	return sdlRenderRect(renderer, rect)
 }
 
+// RenderRect draws a rectangle on the current rendering target at subpixel precision.
+func (renderer *Renderer) RenderRect(rect *FRect) bool {
+	return RenderRect(renderer, rect)
+}
+
 // RenderFillRect fills a rectangle on the current rendering target with the drawing color at subpixel precision.
 func RenderFillRect(renderer *Renderer, rect *FRect) bool {
 	return sdlRenderFillRect(renderer, rect)
+}
+
+// RenderFillRect fills a rectangle on the current rendering target with the drawing color at subpixel precision.
+func (renderer *Renderer) RenderFillRect(rect *FRect) bool {
+	return RenderFillRect(renderer, rect)
 }
 
 // RenderDebugText draws debug text to a [Renderer].
@@ -50,9 +80,19 @@ func RenderDebugText(renderer *Renderer, x, y float32, str string) bool {
 	return sdlRenderDebugText(renderer, x, y, str)
 }
 
+// RenderDebugText draws debug text to a [Renderer].
+func (renderer *Renderer) RenderDebugText(x, y float32, str string) bool {
+	return RenderDebugText(renderer, x, y, str)
+}
+
 // CreateTextureFromSurface creates a texture from an existing surface.
 func CreateTextureFromSurface(renderer *Renderer, surface *Surface) *Texture {
 	return sdlCreateTextureFromSurface(renderer, surface)
+}
+
+// CreateTextureFromSurface creates a texture from an existing surface.
+func (renderer *Renderer) CreateTextureFromSurface(surface *Surface) *Texture {
+	return CreateTextureFromSurface(renderer, surface)
 }
 
 // RenderTexture copies a portion of the texture to the current rendering target at subpixel precision.
@@ -60,9 +100,19 @@ func RenderTexture(renderer *Renderer, texture *Texture, srcrect *FRect, dstrect
 	return sdlRenderTexture(renderer, texture, srcrect, dstrect)
 }
 
+// RenderTexture copies a portion of the texture to the current rendering target at subpixel precision.
+func (renderer *Renderer) RenderTexture(texture *Texture, srcrect *FRect, dstrect *FRect) bool {
+	return RenderTexture(renderer, texture, srcrect, dstrect)
+}
+
 // DestroyTexture destroys the specified texture.
 func DestroyTexture(texture *Texture) {
 	sdlDestroyTexture(texture)
+}
+
+// Destroy destroys the specified texture.
+func (texture *Texture) Destroy() {
+	DestroyTexture(texture)
 }
 
 // func AddVulkanRenderSemaphores(renderer *Renderer, wait_stage_mask uint32, wait_semaphore int64, signal_semaphore int64) bool {
